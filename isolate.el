112699f523c66a4fdc8c14b9963a2d39ce22ad10
Yuan Fu <casouri@gmail.com>
2018-10-02 19:13:09 -0400

    (finally) Fix append and push functions

--- isolate.el
+++ isolate.el
@@ -215,17 +215,17 @@ Return LEFT-SEGMENT itself if not."
 
 ;;;; Fundamental helper
 
-(define-inline isolate--append (seq elt)
+(defsubst isolate--append (seq elt)
   "Append ELT to SEQ destructively."
-  `(if ,seq
-      (nconc ,seq (list ,elt))
-    (setq ,seq (list ,elt))))
+  (if seq
+      (nconc seq (list elt))
+    (setq seq (list elt))))
 
-(define-inline isolate--push (elt seq)
+(defsubst isolate--push (elt seq)
   "Push ELT to SEQ destructively."
-  `(if ,seq
-      (push ,elt ,seq)
-    (setq ,seq (list ,elt))))
+  (if seq
+      (push elt seq)
+    (setq seq (list elt))))
 
 (defun isolate--replace-with (str start end)
   "Replace strings between START and END with STR.
